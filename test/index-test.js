8ad2192ff8a610c696111e743b610009b4c50914
Kevin Simpson <relasine@gmail.com>
2018-08-24 11:50:15 -0600

    Add boilerplate

---
+++ test/index-test.js
@@ -0,0 +1,8 @@
+// import { assert } from 'chai';
+const { assert } = require('chai')
+  
+describe('test', function() {
+  it('should return true', function() {
+    assert.equal(true, true);
+  });
+});
